0b9f7b0b330c0d673c72ec2027f6af2082366add
AHMAD <ahmadou19@gmail.com>
2017-02-02 16:52:08 +0000

    ajout des fonctions create update and delete user

--- model/user/index.js
+++ model/user/index.js
@@ -9,8 +9,8 @@ router.get('/', controller.index);
 router.get('/:id', controller.show);
 
 
-/*router.post('/', controller.create);
-router.put('/:id', controller.update);
+router.post('/', controller.create);
+/*router.put('/:id', controller.update);
 router.patch('/:id', controller.update);
 router.delete('/:id', controller.destroy);*/
 
--- model/user/user.controller.js
+++ model/user/user.controller.js
@@ -2,8 +2,8 @@ var User = require('./user.model');
 
 //liste des userx
 exports.index = function(req, res) {
-  User.findAll().then(function (cycles) {
-    return res.json(cycles);
+  User.findAll().then(function (user) {
+    return res.json(user);
 
 
   });
@@ -11,12 +11,62 @@ exports.index = function(req, res) {
 
 //get user by id
 exports.show = function(req, res) {
-  User.findById(req.params.id).then(function (cycles) {
-    return res.json(cycles);
+  User.findById(req.params.id).then(function (user) {
+    return res.json(user);
 
 
   });
 };
+
+//Creates a new user in the DB.
+exports.create = function(req, res) {
+  User
+    .create(req.body)
+    .then(function (user) {
+      return res.status(201).json(user);
+    })
+    .catch(function (err){
+      if(err) { return handleError(res, err); }
+    });
+};
+
+// Updates an existing user in the DB.
+exports.update = function(req, res) {
+  if(req.body._id) { delete req.body._id; }
+  User
+    .findById(req.params.id)
+    .then(function (user) {
+      if(!user) { return res.status(404).send('Not Found'); }
+      var updated = _.merge(user, req.body);
+      updated.save(function (err) {
+        if (err) { return handleError(res, err); }
+        return res.status(200).json(user);
+      });
+    })
+    .catch(function (err){
+      if (err) { return handleError(res, err); }
+    });
+};
+
+// Deletes a user from the DB.
+exports.destroy = function(req, res) {
+  User
+    .findById(req.params.id)
+    .then(function (user) {
+      if(!user) { return res.status(404).send('Not Found'); }
+      user.destroy(function(err) {
+        if(err) { return handleError(res, err); }
+        return res.status(204).send('No Content');
+      });
+    })
+    .catch(function (err){
+      if(err) { return handleError(res, err); }
+    });
+};
+
+function handleError(res, err) {
+  return res.status(500).send(err);
+}
 
 
 
